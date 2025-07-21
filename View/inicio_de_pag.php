4fba9574d5219ff85b06846107916ffc9c4b5964
JuanEstebanAparicio <apariciojuanesteban@gmail.com>
2025-07-21 14:56:49 -0500

    Diseño de fondo de la portada de la pagina

---
+++ View/inicio_de_pag.php
@@ -0,0 +1,33 @@
+ <!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Restaurante</title>
+    <link rel="stylesheet" href="../CSS/Inicio_pag.css" />
+</head>
+<body>
+  <div id="vanta-bg"></div>
+  <canvas id="emoji-canvas"></canvas>
+  <div class="overlay"></div>
+  <div class="content">
+    <h1 class="animated-title">DELIXIUSYSTEM</h1>
+    <p><span id="typed-text">Trae un servicio confiable</span></p>
+    <button class="btn">Iniciar Sesion</button>
+    <br>
+    <button class="btn2">Registrarse</button>
+  </div>
+
+  <!-- VANTA + THREE -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.waves.min.js"></script>
+  
+  <!-- Efecto maquina de escribir -->
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
+  <!-- Tu script separado -->
+<script src="../JS/Inicio_pag.js"></script>
+</body>
+</html>
