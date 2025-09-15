2266d9f10d4ac9f2dcd2285625bdd4a02188919a
Loldeluis <luismaldonadourib@gmail.com>
2025-09-14 22:49:03 -0500

    Limpieza total de la rama

--- MiRestaurante/controlador/js/tema.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("btnTema").addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
-  });
-  
--- MiRestaurante/controlador/php/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset();  // Borra todas las variables de sesión
-session_destroy(); // Destruye la sesión actual
-
-header("Location: ../../Ver/Login.php"); // Redirige al login
-exit();
-?>
